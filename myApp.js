9ee9443674af824afd22cef2f92fb159a23b0eb3
O-Bernard Ofoegbu <obinnaofoegbu71@gmail.com>
2024-07-14 02:03:21 +0100

    connect mongodb

--- myApp.js
+++ myApp.js
@@ -1,5 +1,13 @@
-require('dotenv').config();
+const mongoose = require("mongoose");
+require("dotenv").config();
 
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB connected..."))
+  .catch((err) => console.log(err));
 
 let Person;
 
